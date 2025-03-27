33fb5f266ac517bfe7a0a54cec12775bc2e26891
Syoma-gif <syomapetrosyan054@gmail.com>
2025-03-27 09:35:56 -0700

    commit

---
+++ goa/level08/classwork/cw02.py
@@ -0,0 +1,5 @@
+#მომხარებელს შემოატანინეთ ორი სიტყვა და მიახდინეთ მათ შორის კონკადინაცია
+
+x = input("Please enter some word: ")
+y = input("Please enter another word: ")
+print(x + y)
---
+++ goa/level17/classwork/cw03.py
@@ -0,0 +1,5 @@
+for i in range(200,250,15):
+    print(i)
+
+for i in range(200, 400):
+    print(i)
