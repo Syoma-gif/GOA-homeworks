5a83e73b27f5c8834ad86c7e546c4a461f4b06b5
Syoma-gif <syomapetrosyan054@gmail.com>
2025-05-13 04:07:43 -0700

    commit

---
+++ goa/level31/cw1.py
@@ -0,0 +1,10 @@
+my_name = "Simon"
+
+user_name = input("Enter your name: ")
+
+
+if my_name.lower() == user_name.lower():
+    print("True")
+else:
+    print("False")
+
---
+++ goa/level31/hw2.py
@@ -0,0 +1,6 @@
+food = ["burger", "pizza", "fries"]  
+
+food.pop()  
+food.append("salad") 
+
+print(food) 
